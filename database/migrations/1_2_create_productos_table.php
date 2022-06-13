63f9e2f87c6ae57917b1e0ca5cc9962f38d0c073
C4RLUUS <carloslorenzo312@gmail.com>
2022-06-13 17:12:49 +0200

    bug

--- database/migrations/1_2_create_productos_table.php
+++ database/migrations/1_2_create_productos_table.php
@@ -18,12 +18,12 @@ return new class extends Migration
             $table->string('nombre'); 
             $table->text('descripcion'); 
             $table->float('precio'); 
-            $table->integer('stock'); 
             $table->text('imagen'); 
             $table->boolean('dulce'); 
             $table->boolean('salado');
             $table->boolean('frio'); 
             $table->boolean('caliente'); 
+            $table->integer('stock'); 
             $table->integer('low_stock_alert'); 
             $table->boolean('active')->default(1); 
             $table->boolean('deleted')->default(0); 
